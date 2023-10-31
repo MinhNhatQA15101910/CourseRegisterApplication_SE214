915a6175cd483deb24f4c9b2c88b47278ea3c68c
Nhatduy13nhatduy <nhatduy13nhatduy@gmail.com>
2023-11-01 03:41:41 +0700

    Update

--- CourseRegisterApplication.MAUI/ViewModels/AdminViewModels/AdminMenuViewModel.cs
+++ CourseRegisterApplication.MAUI/ViewModels/AdminViewModels/AdminMenuViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace CourseRegisterApplication_SE214.ViewModels.AdminViewModels
+﻿using System.Collections.ObjectModel;
+namespace CourseRegisterApplication_SE214.ViewModels.AdminViewModels
 {
-    public class AdminMenuViewModel
-    {
-    }
+	public class MenuItems
+	{
+		public string Title { get; set; }
+		public string Icon { get; set; }
+		public Color Color { get; set; }
+	}
+
+	public class AdminFlyoutItem
+	{
+		public ObservableCollection<MenuItems> AdminFlyoutItems { get; } = new ObservableCollection<MenuItems>();
+		public AdminFlyoutItem()
+		{
+			AdminFlyoutItems.Add(new MenuItems { Title = "Dashboard", Icon = "home.png", Color = Color.FromArgb("#152259") });
+			AdminFlyoutItems.Add(new MenuItems { Title = "Accountant account", Icon = "student.png", Color = Color.FromArgb("#152259") });
+			AdminFlyoutItems.Add(new MenuItems { Title = "Student account", Icon = "student.png", Color = Color.FromArgb("#152259") });
+			AdminFlyoutItems.Add(new MenuItems { Title = "Admin account", Icon = "book.png", Color = Color.FromArgb("#152259") });
+		}
+
+	}
 }
--- CourseRegisterApplication.MAUI/Views/AdminViews/AdminMenuItem.xaml.cs
+++ CourseRegisterApplication.MAUI/Views/AdminViews/AdminMenuItem.xaml.cs
@@ -1,9 +1,40 @@
+using CourseRegisterApplication_SE214.ViewModels.AdminViewModels;
+
 namespace CourseRegisterApplication_SE214.Views.AdminViews;
 
 public partial class AdminMenuItem : ContentPage
 {
+	public event EventHandler<Type> PageChanged;
 	public AdminMenuItem()
 	{
 		InitializeComponent();
+		BindingContext = new AdminFlyoutItem();
+	}
+	private void OnItemTapped(object sender, ItemTappedEventArgs e)
+	{
+		if (e.Item is MenuItems menuItem)
+		{
+			Type pageType = null;
+
+			if (menuItem.Title == "Dashboard")
+			{
+				pageType = typeof(AdminDashboardPage);
+			}
+			else if (menuItem.Title == "Accountant account")
+			{
+				pageType = typeof(EmployeeAccountManagementPage);
+			}
+			else if (menuItem.Title == "Student account")
+			{
+				pageType = typeof(StudentAccountManagementPage);
+			}
+			else if (menuItem.Title == "Admin account")
+			{
+				pageType = typeof(AdminAccountManagementPage);
+			}
+
+
+			PageChanged?.Invoke(this, pageType);
+		}
 	}
 }
