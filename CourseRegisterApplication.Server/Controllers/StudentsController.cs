0e4d26e0de4720acaaa498d2e8f6b7a7c59e293a
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2023-11-09 16:22:57 +0700

    update UserController, add StudentsController

---
+++ CourseRegisterApplication.Server/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using CourseRegisterApplication.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseRegisterApplication.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly CourseRegisterManagementDbContext _context;
+
+        public StudentsController(CourseRegisterManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<Student[]>> GetAllStudent()
+        {
+            if (_context.Students == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var students = await _context.Students.ToListAsync();
+
+            if (students == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return Ok(students);
+        }
+    }
+}
--- CourseRegisterApplication.Server/Controllers/UsersController.cs
+++ CourseRegisterApplication.Server/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CourseRegisterApplication.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+#pragma warning disable CS8600 
 
 namespace CourseRegisterApplication.Server.Controllers
 {
@@ -29,5 +32,66 @@ namespace CourseRegisterApplication.Server.Controllers
 
             return new OkObjectResult(user);
         }
+
+        // Displaying accounts based on roleId 1 - admin, 2 - accountant, 3 - student
+        [HttpGet()]
+        public async Task<ActionResult<User[]>> GetAccountsByRole([FromQuery] int roleId)
+        {
+            try
+            {
+                // Check if the 'roleId' query parameter is provided.
+                if (string.IsNullOrEmpty(roleId.ToString()))
+                {
+                    return BadRequest("Role ID is required.");
+                }
+
+                var accounts = await _context.Users.Where(u => u.RoleId == roleId).ToListAsync();
+
+                if (accounts == null)
+                {
+                    return NotFound("No accounts found!");
+                }
+
+                // Create custom responses with adjusted properties
+                IEnumerable<object> response = null;
+                switch (roleId)
+                {
+                    case 1:
+                        response = accounts.Select(s => new
+                        {
+                            id = s.Id,
+                            username = s.Username,
+                            email = s.Email,
+                            accountType = "Admin"
+                        });
+                        break;
+                    case 2:
+                        response = accounts.Select(s => new
+                        {
+                            id = s.Id,
+                            username = s.Username,
+                            email = s.Email,
+                            accountType = "Accountant"
+                        });
+                        break;
+                    case 3:
+                        response = accounts.Select(s => new
+                        {
+                            id = s.Id,
+                            studendSpecificId = s.Username,
+                            email = s.Email,
+                        });
+                        break;
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
+            }
+        }
+
+
     }
 }
