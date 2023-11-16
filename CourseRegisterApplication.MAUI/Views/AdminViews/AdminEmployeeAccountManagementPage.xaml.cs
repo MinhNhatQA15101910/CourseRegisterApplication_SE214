3e934699268e1f6612ce3c3711a347ad14a5319f
JustVvu <nguyenvanvu065@gmail.com>
2023-11-16 23:05:22 +0700

    Design UI for Student and Accountant Account Manager in AdminView

--- CourseRegisterApplication.MAUI/Views/AdminViews/AdminEmployeeAccountManagementPage.xaml.cs
+++ CourseRegisterApplication.MAUI/Views/AdminViews/AdminEmployeeAccountManagementPage.xaml.cs
@@ -5,5 +5,34 @@ public partial class AdminEmployeeAccountManagementPage : ContentPage
 	public AdminEmployeeAccountManagementPage()
 	{
 		InitializeComponent();
-	}
+        collectionView.ItemsSource = GetAllStudents();
+    }
+
+    public List<Students> GetAllStudents()
+    {
+        return new List<Students>
+        {
+            new Students("Nguyen Van A", "111111", "123456"),
+            new Students("Nguyen Van B", "222222", "456789"),
+            new Students("Nguyen Van C", "333333", "789123"),
+            new Students("Nguyen Van D", "444444", "147258"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369"),
+            new Students("Nguyen Van E", "555555", "258369")
+        };
+    }
 }
---
+++ CourseRegisterApplication.Shared/Students.cs
@@ -0,0 +1,16 @@
+﻿namespace CourseRegisterApplication.Shared
+{
+    public class Students
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Phone { get; set; }
+
+        public Students(string name, string id, string phone)
+        {
+            Name = name;
+            Id = id;
+            Phone = phone;
+        }
+    }
+}
