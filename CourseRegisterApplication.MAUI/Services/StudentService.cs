e05fcce8330a9f0cfa11e465cca267f9563fbc15
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2023-11-28 09:10:02 +0700

    finish sprint 2

--- CourseRegisterApplication.MAUI/Services/StudentService.cs
+++ CourseRegisterApplication.MAUI/Services/StudentService.cs
@@ -7,8 +7,10 @@ namespace CourseRegisterApplication.MAUI.Services
     public class StudentService : IStudentService
     {
         private readonly string _baseUrl = "https://localhost:7182/api/Students/";
+        private readonly string _branchUrl = "https://localhost:7182/api/Branches/";
         private readonly HttpClient _httpClient = new HttpClient();
         private List<Student> _students;
+        private List<Branch> _branches;
 
         public async Task<List<Student>> GetAllStudent()
         {
@@ -16,9 +18,9 @@ namespace CourseRegisterApplication.MAUI.Services
             {
                 string studentsApiUrl = $"{_baseUrl}";
 
-                using (HttpClient httpClient = new HttpClient())
+                using (_httpClient)
                 {
-                    var studentsResponse = await httpClient.GetAsync(new Uri(studentsApiUrl)).ConfigureAwait(false);
+                    var studentsResponse = await _httpClient.GetAsync(new Uri(studentsApiUrl)).ConfigureAwait(false);
 
                     if (studentsResponse.IsSuccessStatusCode)
                     {
@@ -39,16 +41,47 @@ namespace CourseRegisterApplication.MAUI.Services
                 return null;
             }
         }
+        public async Task<List<Branch>> GetAllBranches()
+        {
+            try
+            {
+                string branchesApiUrl = $"{_branchUrl}";
+
+                using (_httpClient)
+                {
+                    var branchesResponse = await _httpClient.GetAsync(new Uri(branchesApiUrl)).ConfigureAwait(false);
+
+                    if (branchesResponse.IsSuccessStatusCode)
+                    {
+                        string branchJsonResponse = await branchesResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                        var branches = JsonConvert.DeserializeObject<List<Branch>>(branchJsonResponse);
+
+                        _branches = branches.ToList();
+                        return _branches;
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
 
         public List<Student> FilterBySearchBox(string filter)
         {
-            return _students.Where(u => u.StudentSpecificId.Contains(filter)).ToList();
+            return _students
+                .Where(u => u.StudentSpecificId.Contains(filter) || _branches.Any(b => b.Id == u.BranchId && b.BranchName.Contains(filter)))
+                .ToList();
         }
-        public List<Student> FilterUserAZByStudentSpecificId()
+        public List<Student> FilterStudentAZByStudentSpecificId()
         {
             return _students.OrderBy(u => u.StudentSpecificId).ToList();
         }
-        public List<Student> FilterUserZAByStudentSpecificId()
+        public List<Student> FilterStudentZAByStudentSpecificId()
         {
             return _students.OrderByDescending(u => u.StudentSpecificId).ToList();
         }
---
+++ CourseRegisterApplication.Server/Controllers/BranchesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CourseRegisterApplication.Server;
+using CourseRegisterApplication.Shared;
+
+namespace CourseRegisterApplication.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchesController : ControllerBase
+    {
+        private readonly CourseRegisterManagementDbContext _context;
+
+        public BranchesController(CourseRegisterManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Branches
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Branch>>> GetBranches()
+        {
+          if (_context.Branches == null)
+          {
+              return NotFound();
+          }
+            return await _context.Branches.ToListAsync();
+        }
+
+        // GET: api/Branches/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Branch>> GetBranch(int id)
+        {
+          if (_context.Branches == null)
+          {
+              return NotFound();
+          }
+            var branch = await _context.Branches.FindAsync(id);
+
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return branch;
+        }
+
+        // PUT: api/Branches/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBranch(int id, Branch branch)
+        {
+            if (id != branch.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(branch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Branches
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Branch>> PostBranch(Branch branch)
+        {
+          if (_context.Branches == null)
+          {
+              return Problem("Entity set 'CourseRegisterManagementDbContext.Branches'  is null.");
+          }
+            _context.Branches.Add(branch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBranch", new { id = branch.Id }, branch);
+        }
+
+        // DELETE: api/Branches/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBranch(int id)
+        {
+            if (_context.Branches == null)
+            {
+                return NotFound();
+            }
+            var branch = await _context.Branches.FindAsync(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            _context.Branches.Remove(branch);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BranchExists(int id)
+        {
+            return (_context.Branches?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
