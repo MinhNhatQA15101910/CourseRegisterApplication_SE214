b067833c2379bcb08f3a0d5c4b2f57edef0d90db
Do Nhat <dmnhat.pt@gmail.com>
2024-01-06 01:40:56 +0700

    Update course registration form state to expired.

--- CourseRegisterApplication.MAUI/App.xaml.cs
+++ CourseRegisterApplication.MAUI/App.xaml.cs
@@ -1,14 +1,42 @@
-﻿using CourseRegisterApplication.MAUI.Views;
+﻿using CourseRegisterApplication.MAUI.IServices;
+using CourseRegisterApplication.MAUI.Views;
 
 namespace CourseRegisterApplication.MAUI
 {
     public partial class App : Application
     {
-        public App(LoginPage page)
+        private readonly ISemesterService _semesterService;
+        private readonly ICourseRegistrationFormService _courseRegistrationFormService;
+
+        public App(LoginPage page, ICourseRegistrationFormService courseRegistrationFormService, ISemesterService semesterService)
         {
             InitializeComponent();
 
+            _courseRegistrationFormService = courseRegistrationFormService;
+            _semesterService = semesterService;
+
             MainPage = page;
+        }
+
+        private async Task UpdateCourseRegistrationFormStateToExpired()
+        {
+            List<CourseRegistrationForm> courseRegistrationFormList = await _courseRegistrationFormService.GetAllCourseRegistrationForm();
+            foreach (CourseRegistrationForm form in courseRegistrationFormList)
+            {
+                Semester semester = await _semesterService.GetSemesterById(form.SemesterId);
+                if (form.State != CourseRegistrationFormState.Expired && semester.EndRegistrationDate < DateTime.Now)
+                {
+                    form.State = CourseRegistrationFormState.Expired;
+                    await _courseRegistrationFormService.UpdateCourseRegistrationForm(form.Id, form);
+                }
+            }
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+
+            await UpdateCourseRegistrationFormStateToExpired();
         }
     }
 }
