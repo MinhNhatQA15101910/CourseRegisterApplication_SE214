5e0a896b7c766ce4b619c1f42a7dd3c9d438eaea
Do Nhat <dmnhat.pt@gmail.com>
2024-01-05 23:45:46 +0700

    Done Available Courses Management.

--- CourseRegisterApplication.Server/Controllers/SemestersController.cs
+++ CourseRegisterApplication.Server/Controllers/SemestersController.cs
@@ -51,7 +51,7 @@
 
                 if (name.HasValue && !string.IsNullOrEmpty(year.ToString()))
                 {
-                    querySemester = semesters.FirstOrDefault(s => s.SemesterName == name);
+                    querySemester = semesters.Find(s => s.SemesterName == name);
                 }
 
                 if (querySemester == null)
@@ -65,6 +65,73 @@
             {
                 return StatusCode(500, $"Internal server error: {ex}");
             }
+        }
+
+        [HttpGet("current")]
+        public async Task<ActionResult<Semester>> GetCurrentSemester()
+        {
+            if (_context.Semesters == null)
+            {
+                return NotFound();
+            }
+            var semester = await _context.Semesters
+                .FromSqlRaw(
+                    "SELECT * " +
+                    "FROM dbo.Semesters s " +
+                    "WHERE s.Id = " +
+                        "(SELECT MAX(Id) " +
+                        "FROM dbo.Semesters)"
+                )
+                .FirstAsync();
+
+            return Ok(semester);
+        }
+
+        [HttpPut("{semesterId}")]
+        public async Task<IActionResult> PutSemester(int semesterId, Semester semester)
+        {
+            if (semesterId != semester.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(semester).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SemesterExists(semesterId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Semester>> PostSemester(Semester semester)
+        {
+            if (_context.Branches == null)
+            {
+                return Problem("Entity set 'CourseRegisterManagementDbContext.Semesters'  is null.");
+            }
+            _context.Semesters.Add(semester);
+            await _context.SaveChangesAsync();
+
+            return Ok(semester);
+        }
+
+        private bool SemesterExists(int semesterId)
+        {
+            return (_context.Semesters?.Any(p => p.Id == semesterId)).GetValueOrDefault();
         }
     }
 }
